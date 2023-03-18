41722459cf75be4a3882878f2ee85ae2f23571bc
david-allan-jones <davej1094@yahoo.com>
2023-03-18 17:01:00 +0900

    Create a first draft of layout and home page.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,38 @@
-import { Footer } from "./Footer";
-import { Header } from "./Header";
+import Head from 'next/head';
+import React from "react";
+import styles from '@/styles/Layout.module.css'
 
 export function Layout(props: React.PropsWithChildren) {
-    return <div>
-        <Header />
-        <section>
-            {props.children}
-        </section>
-        <Footer />
-    </div>
+    return (
+        <div>
+            <Head>
+                <title>Snapvote</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device=width" />
+                <link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet" />
+            </Head>
+            <section className={styles.contentWrapper}> 
+                {props.children}
+            </section>
+            <footer className={styles.footer}>
+                <div className={styles.flexContainer}>
+                    <div className={styles.flexItem}>
+                        <p>AUTHOR</p>
+                        <p>David Jones</p>
+                    </div>
+                    <div className={styles.flexItem}>
+                        <p>LINKS</p>
+                        <div>
+                            <a href="https://github.com/david-allan-jones/snapvote">Source Code</a>
+                        </div>
+                        <div>
+                            <a href="https://davidjonesdev.com/contact">Contact Author</a>
+                        </div>
+                    </div>
+                </div>        
+            </footer>
+        </div>
+    );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import { Layout } from "@/components/Layout"
 import Link from "next/link"
+import styles from '@/styles/Home.module.css'
 
 export default function IndexPage() {
     return <Layout>
-        <p>Welcome to Snapvote!</p>
-        <Link href="/polls">Start a new poll</Link>
+        <div className={styles.container}>
+            <p className={styles.noMargin}>Welcome to</p>
+            <p className={styles.snapvote}>Snapvote!</p>
+            <Link className={styles.link} href="/polls">
+                <div className={styles.primaryBtn}>
+                    Start a new poll
+                </div>
+            </Link>
+        </div>
     </Layout>
 }
