b6764584102cd43aa54b16f04ad896cf53fc2439
David <80140291+david-allan-jones@users.noreply.github.com>
2023-04-19 22:18:34 +0900

    Add i18n support (#13)

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '@/styles/globals.css'
 import "react-datepicker/dist/react-datepicker.css";
 import { Poppins } from 'next/font/google'
+import { appWithTranslation } from 'next-i18next';
 
 const poppins = Poppins({
     weight: '800',
     subsets: ['latin'],
 })
 
-export default function MyApp({ Component, pageProps }) {
+const MyApp = function({ Component, pageProps }) {
     return <main className={poppins.className}>
         <Component {...pageProps} />
     </main>
 }
+
+export default appWithTranslation(MyApp)
--- pages/polls/index.tsx
+++ pages/polls/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useEffect, useRef, useState } from "react"
+import { FormEvent, useEffect, useRef, useState } from "react"
 import { Option } from "@/components/Option"
 import { Layout } from "@/components/Layout"
 import { WritePollResponseData } from "../api/polls"
@@ -7,6 +7,9 @@ import DatePicker from 'react-datepicker'
 import { LoadingSpinner } from "@/components/LoadingAnimation"
 import styles from '@/styles/Home.module.css'
 import { OPTION_LEN_LIMIT, OPTION_LIMIT, QUESTION_LEN_LIMIT } from "@/utils/consts"
+import { useTranslation } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import { useRouter } from "next/router"
 
 const ONE_DAY = 86400000
 
@@ -21,6 +24,9 @@ export default function IndexPage() {
     const questionInputRef = useRef<any>(null)
     const optionInputRef = useRef<any>(null)
 
+    const { t } = useTranslation('common')
+    const router = useRouter()
+
     useEffect(() => {
         if (questionInputRef.current) {
             questionInputRef.current.focus()
@@ -30,11 +36,11 @@ export default function IndexPage() {
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault()
         if (question.length === 0 || question.length > QUESTION_LEN_LIMIT) {
-            setErrorMessage('Questions must be between 1-100 characters.')
+            setErrorMessage(t('common.questionLengthError').toString())
             return
         }
         if (savedOptions.length < 2) {
-            setErrorMessage('You need to provide at least 2 options a poll.')
+            setErrorMessage(t('common.notEnoughOptionsError').toString())
             return
         }
 
@@ -56,13 +62,13 @@ export default function IndexPage() {
             if (error) {
                 setErrorMessage(error.message)
             } else {
-                setErrorMessage('There was a problem creating your poll. Please wait and try again later.')
+                setErrorMessage(t('common.genericPostError').toString())
             }
             return
         }
         const tokenStore = createCreatorTokenStore()
         tokenStore.append(payload?.id as string, payload?.creator_token as string)
-        window.location.href = `/polls/${payload?.id}`
+        router.push(`/polls/${payload?.id}`)
     }
 
     const handleQuestionChange = (e: { target: { value: string }}) => {
@@ -77,15 +83,15 @@ export default function IndexPage() {
 
     const handleAddOption = () => {
         if (savedOptions.length === OPTION_LIMIT) {
-            setErrorMessage('You may only enter a max of 10 options')
+            setErrorMessage(t('common.maxOptionsError').toString())
             return
         }
         if (option === '') {
-            setErrorMessage('You may not use an empty string as an option')
+            setErrorMessage(t('common.noEmptyStringOptionError').toString())
             return
         }
         if (option.length > OPTION_LEN_LIMIT) {
-            setErrorMessage('You may only use options that are 100 characters or less')
+            setErrorMessage(t('common.optionLengthError').toString())
             return
         }
         setSavedOptions([...savedOptions, option])
@@ -107,9 +113,9 @@ export default function IndexPage() {
 
     return <Layout>
         <form className={`${styles.form} darkgray-bg`} typeof="submit" action="/api/polls" method="post" onSubmit={handleSubmit}>
-            <p>Input your poll data</p>
+            <p>{t('common.inputPollData')}</p>
             <div className={styles.grid}>
-                <span className={styles.questionPrompt}>Question:</span>
+                <span className={styles.questionPrompt}>{t('common.questionLabel')}</span>
                 <input
                     ref={questionInputRef}
                     type="text"
@@ -128,7 +134,7 @@ export default function IndexPage() {
                     />
                 ))}
                 </div>
-                <span className={styles.optionPrompt}>Option:</span>
+                <span className={styles.optionPrompt}>{t('common.optionLabel')}</span>
                 <input
                     ref={optionInputRef}
                     type="text"
@@ -145,16 +151,30 @@ export default function IndexPage() {
                 >
                     +
                 </button>
-                <span className={styles.datePrompt}>Expiration Date:</span>
+                <span className={styles.datePrompt}>{t('common.dateLabel')}</span>
                 <div className={styles.dateInput}>
                     <DatePicker selected={expires} onChange={(date: Date) => setExpires(date)} />
                 </div>
             </div>
-            <input className={`${styles.primaryBtn} full-width`} type="submit" value="Create" />
+            <input
+                className={`${styles.primaryBtn} full-width`}
+                type="submit"
+                value={t('common.createButtonLabel').toString()}
+            />
             <p>{errorMessage}</p>
         </form>
         <div className={styles.horizontalCenter}>
             {loading && <LoadingSpinner />}
         </div>
     </Layout>
+}
+
+export async function getStaticProps(context: { locale: string }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(context.locale, [
+                'common',
+            ])),
+        }
+    }
 }
