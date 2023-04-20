6c3b074255109220ace9952a5e428ecdbc321649
david-allan-jones <davej1094@yahoo.com>
2023-04-20 23:14:29 +0900

    Add chart to answer page.

--- components/polls/ResultChart.tsx
+++ components/polls/ResultChart.tsx
@@ -1,5 +1,5 @@
 import { Option } from "@/store/redis"
-import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
+import { Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 
 type Props = {
     options: Option[]
@@ -21,16 +21,17 @@ function mapToChart(options: Option[]) {
 
 export default function ResultChart(props: Props) {
     return (
-        <ResponsiveContainer width={300} height={300}>
+        <ResponsiveContainer width='100%' height={props.options.length * 50}>
             <BarChart 
                 barSize={24}
                 style={{ fill: 'white' }}
                 data={mapToChart(props.options)}
                 layout="vertical"
             >
-                <XAxis stroke='#cecece' style={{ fill: 'white' }} type="number" tick={true} tickCount={1} />
+                <XAxis tick={{ width: 10 }} stroke='#cecece' style={{ fill: 'white' }} type="number" tick={true} tickCount={1} />
                 <YAxis stroke='#cecece' style={{ fill: 'white' }} dataKey="Answer" type="category" />
                 <Tooltip wrapperStyle={{color: 'black'}} itemStyle={{color: 'black'}}/>
+                <Legend />
                 <Bar dataKey="Count" fill="#FFB200" />
             </BarChart>
         </ResponsiveContainer>
