5b5eeca375df2295c0126e78bd1a4c400eedd08d
david-allan-jones <davej1094@yahoo.com>
2023-04-23 11:17:26 +0900

    Replace + with undescore in base64 system to make it url safe.

--- utils/base64.ts
+++ utils/base64.ts
@@ -1,5 +1,5 @@
 export function generateRandomBase64String(n: number): string {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-';
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
     let result = '';
   
     for (let i = 0; i < n; i++) {
