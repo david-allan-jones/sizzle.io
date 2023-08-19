c4d8e2b17e91939c737cc7fc1c3c028814d0094a
David <80140291+david-allan-jones@users.noreply.github.com>
2023-08-19 11:25:58 +0900

    Add cypress (#21)

--- components/Option.tsx
+++ components/Option.tsx
@@ -4,8 +4,9 @@ import styles from '@/styles/Home.module.css'
 export function Option(props: { value: string, onDelete: () => void }) {
     const option = useMemo(() => props.value, [props.value])
     
-    return <div style={{ textAlign: 'left', marginBottom: '5px', display: 'flex', flexDirection: 'row', flexWrap: 'nowrap' }}>
+    return <div data-test='saved-option' style={{ textAlign: 'left', marginBottom: '5px', display: 'flex', flexDirection: 'row', flexWrap: 'nowrap' }}>
         <button
+            data-test='saved-option-delete-btn'
             className={styles.secondaryBtn}
             type="button"
             onClick={props.onDelete}
@@ -16,7 +17,7 @@ export function Option(props: { value: string, onDelete: () => void }) {
         <div style={{
             overflowWrap: 'anywhere',
         }}>
-            <p style={{ fontFamily: 'arial' }}>
+            <p data-test='saved-option-text' style={{ fontFamily: 'arial' }}>
                 {option}
             </p>
         </div>
---
+++ cypress/e2e/polls.cy.js
@@ -0,0 +1,144 @@
+describe('polls.cy.js', () => {
+    beforeEach(() => {
+        cy.visit('localhost:3000/polls')
+    })
+
+    it('should have static input prompts and buttons', () => {
+        cy.get('[data-test="input-prompt"]')
+            .should('have.text', 'Input your poll data')
+
+        // Question, option, date inputs
+        cy.get('input[type="text"]')
+            .should('have.length', 3)
+
+        // Question, option, date prompts
+        cy.get('[data-test="question-prompt"]')
+            .should('have.text', 'Question:')
+        cy.get('[data-test="option-prompt"]')
+            .should('have.text', 'Option:')
+        cy.get('[data-test="expiration-date-prompt"]')
+            .should('have.text', 'Expiration Date:')
+
+        // Submit button
+        cy.get('input[type="submit"]')
+            .should('have.value', 'Create')
+    })
+
+    it('should let you type in question', () => {
+        const value = 'abc'
+        cy.get('[data-test="question-input"]')
+            .type(value)
+            .should('have.value', value)
+    })
+
+    it('should be able to create options', () => {
+        const value = 'abc'
+        cy.get('[data-test="option-input"]')
+            .type(value)
+            .type('{enter}')
+            .type(value)
+
+        cy.get('[data-test="add-option-btn"]')
+            .click()
+
+        cy.get('[data-test="saved-option"]')
+            .should('have.length', 2)
+    })
+
+    it('should allow you to delete options after typing', () => {
+        const value = 'abc'
+        cy.get('[data-test="option-input"]')
+            .type(value)
+            .type('{enter}')
+            .type(value)
+            .type('{enter}')
+
+        cy.get('[data-test="saved-option-delete-btn"]')
+            .eq(1)
+            .click()
+
+        cy.get('[data-test="saved-option"]')
+            .should('have.length', 1)
+    })
+
+    it('should not save more than 10 options', () => {
+        const value = 'abc'
+        for (let i = 0; i < 11; i++) {
+            cy.get('[data-test="option-input"]')
+                .type(value)
+                .type('{enter}')
+        }
+        cy.get('[data-test="saved-option"]')
+            .should('have.length', 10)
+    })
+
+    it('should show error when trying to submit >10 option', () => {
+        const value = 'abc'
+        for (let i = 0; i < 11; i++) {
+            cy.get('[data-test="option-input"]')
+                .type(value)
+                .type('{enter}')
+        }
+
+        cy.get('[data-test="error"]')
+            .should('have.text', 'You may only enter a max of 10 options')
+    })
+
+    it('should show error when trying to submit only 1 option', () => {
+        const value = 'abc'
+        cy.get('[data-test="question-input"]')
+            .type('test')
+
+        cy.get('[data-test="option-input"]')
+            .type(value)
+            .type('{enter}')
+
+        cy.get('input[type="submit"]')
+            .click()
+
+        cy.get('[data-test="error"]')
+            .should('have.text', 'You need to provide at least 2 options a poll.')
+    })
+
+    it('should show error if you try to submit long question', () => {
+        cy.get('[data-test="question-input"]')
+            .type('x'.repeat(101))
+
+        cy.get('input[type="submit"]')
+            .click()
+
+        cy.get('[data-test="error"]')
+            .should('have.text', 'Questions must be between 1-100 characters.')
+    })
+
+    it('should show error if you try to save an empty option', () => {
+        cy.get('[data-test="option-input"]')
+            .type('{enter}')
+
+        cy.get('[data-test="error"]')
+            .should('have.text', 'You may not use an empty string as an option')
+    })
+
+    it('should show error if you try to type long options', () => {
+        cy.get('[data-test="option-input"]')
+            .type('x'.repeat(101))
+            .type('{enter}')
+
+        cy.get('[data-test="error"]')
+            .should('have.text', 'You may only use options that are 100 characters or less')
+    })
+
+    it('happy path', () => {
+        cy.get('[data-test="question-input"]')
+            .type('test question')
+        cy.get('[data-test="option-input')
+            .type('option 1')
+            .type('{enter}')
+            .type('option 2')
+            .type('{enter}')
+        cy.get('input[type="submit"]')
+            .click()
+
+        cy.url().should('match', /polls\/[\w\-\_]+/)
+    })
+})
